8cc175be0b2ed348280d4cddbeb17610dce239b5
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2021-12-03 19:58:39 +0200

    Update Calculator.java

--- Calculator/src/main/java/Calculator/Calculator.java
+++ Calculator/src/main/java/Calculator/Calculator.java
@@ -343,7 +343,7 @@ public class Calculator extends javax.swing.JFrame {
     }                                          
 
     private void dotButtonActionPerformed(java.awt.event.ActionEvent evt) {                                          
-        // Якщо точка ще не була введена, то дописати точку
+        // If there was no point, write point:
         if (!screen.getText().contains(".")) {
             screen.setText(screen.getText() + evt.getActionCommand());
         }
@@ -390,11 +390,6 @@ public class Calculator extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
